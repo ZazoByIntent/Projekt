3de92ce17f384fc8602a95edd07e4a682bf426e9
Aljaž Hafner <87764692+ZazoByIntent@users.noreply.github.com>
2022-05-02 12:11:26 +0200

    Created application base, defined standard UI, added MyApplication class

--- mobileApp/app/src/main/java/com/example/npo_agregat/MainActivity.kt
+++ mobileApp/app/src/main/java/com/example/npo_agregat/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.npo_agregat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.npo_agregat.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    lateinit var app:MyApplication
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        app = application as MyApplication
+
+        binding.btnStart.setOnClickListener {
+            if(binding.tvStatus.text == getString(R.string.textview_status_idle)){
+                binding.tvStatus.text = getString(R.string.textview_status_capturing)
+                // Zacni zajemanje podatkov
+            }
+        }
+        binding.btnStop.setOnClickListener {
+            if(binding.tvStatus.text == getString(R.string.textview_status_capturing)){
+                binding.tvStatus.text = getString(R.string.textview_status_idle)
+                // Prenehaj zajemanje podatkov
+            }
+        }
     }
 }
