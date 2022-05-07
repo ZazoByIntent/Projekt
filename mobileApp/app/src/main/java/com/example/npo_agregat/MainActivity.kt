f1dc5d4e3a327f13b038ed09dbca0c8d41afbab1
DinoDinoo93 <57443334+DinoDinoo93@users.noreply.github.com>
2022-05-07 21:16:53 +0200

    Dodana senzorja za pospešek in žiroskop
    Dodal sem senzorja za žiroskop in pospešek, za lokacijo sem nastavo da se ustavi senzor, ko klikneš stop, za ta druga dva nevem kok

--- mobileApp/app/src/main/java/com/example/npo_agregat/MainActivity.kt
+++ mobileApp/app/src/main/java/com/example/npo_agregat/MainActivity.kt
@@ -1,14 +1,24 @@
 package com.example.npo_agregat
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Half.EPSILON
 import android.util.Log
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.npo_agregat.databinding.ActivityMainBinding
 import com.google.android.gms.location.*
+import kotlin.math.cos
+import kotlin.math.sin
+import kotlin.math.sqrt
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -16,6 +26,11 @@ class MainActivity : AppCompatActivity() {
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var locationRequest: LocationRequest
     private lateinit var locationCallback: LocationCallback
+    private lateinit var sensorManager: SensorManager
+    private val NS2S = 1.0f / 1000000000.0f
+    private val deltaRotationVector = FloatArray(4) { 0f }
+    private var timestamp: Float = 0f
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,24 +38,134 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         app = application as MyApplication
 
+        if ((ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) && (ContextCompat.checkSelfPermission(this, Manifest.permission.HIGH_SAMPLING_RATE_SENSORS) != PackageManager.PERMISSION_GRANTED)) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.HIGH_SAMPLING_RATE_SENSORS), 2)
+        }
+
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        createLocationRequest()
+
+
 
         binding.btnStart.setOnClickListener {
             if(binding.tvStatus.text == getString(R.string.textview_status_idle)){
                 binding.tvStatus.text = getString(R.string.textview_status_capturing)
                 // Zacni zajemanje podatkov
-                startLocationUpdates();
+                startLocationUpdates() //Začne slediti lokacija
+                setUpSensorStuff()
             }
         }
         binding.btnStop.setOnClickListener {
             if(binding.tvStatus.text == getString(R.string.textview_status_capturing)){
                 binding.tvStatus.text = getString(R.string.textview_status_idle)
                 // Prenehaj zajemanje podatkov
+                stopLocationUpdates();
             }
         }
     }
 
-    private fun getLocationUpdates()
+    private fun setUpSensorStuff() {
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+
+        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also { accelerometer ->
+            sensorManager.registerListener(
+                mAcceleratorSensorListener,
+                accelerometer,
+                SensorManager.SENSOR_DELAY_UI,
+                SensorManager.SENSOR_DELAY_UI
+            )
+        }
+
+        sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)?.also { gyroscope ->
+            sensorManager.registerListener(
+                mGyroscopeSensorListener,
+                gyroscope,
+                SensorManager.SENSOR_DELAY_UI,
+                SensorManager.SENSOR_DELAY_UI
+            )
+        }
+
+    }
+
+    private val mAcceleratorSensorListener: SensorEventListener = object : SensorEventListener {
+        override fun onSensorChanged(event: SensorEvent) {
+            // In this example, alpha is calculated as t / (t + dT),
+            // where t is the low-pass filter's time-constant and
+            // dT is the event delivery rate.
+
+            val alpha: Float = 0.8f
+            // Isolate the force of gravity with the low-pass filter.
+            val gravity = arrayOf(0.0, 0.0, 0.0)
+            val linear_acceleration = arrayOf(0.0, 0.0, 0.0)
+            gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0]
+            gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1]
+            gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2]
+
+            // Remove the gravity contribution with the high-pass filter.
+
+            linear_acceleration[0] = event.values[0] - gravity[0]
+            linear_acceleration[1] = event.values[1] - gravity[1]
+            linear_acceleration[2] = event.values[2] - gravity[2]
+            Log.e("X Speed: ", linear_acceleration[0].toString())
+            //Log.e("Acceleration 1: ", linear_acceleration[1].toString())
+            //Log.e("Acceleration 2: ", linear_acceleration[2].toString())
+        }
+
+        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
+
+        }
+    }
+
+    private val mGyroscopeSensorListener: SensorEventListener = object : SensorEventListener {
+        override fun onSensorChanged(event: SensorEvent) {
+            // This timestep's delta rotation to be multiplied by the current rotation
+            // after computing it from the gyro sample data.
+            if (timestamp != 0f && event != null) {
+                val dT = (event.timestamp - timestamp) * NS2S
+                // Axis of the rotation sample, not normalized yet.
+                var axisX: Float = event.values[0]
+                var axisY: Float = event.values[1]
+                var axisZ: Float = event.values[2]
+
+                // Calculate the angular speed of the sample
+                val omegaMagnitude: Float = sqrt(axisX * axisX + axisY * axisY + axisZ * axisZ)
+
+                // Normalize the rotation vector if it's big enough to get the axis
+                // (that is, EPSILON should represent your maximum allowable margin of error)
+                if (omegaMagnitude > EPSILON) {
+                    axisX /= omegaMagnitude
+                    axisY /= omegaMagnitude
+                    axisZ /= omegaMagnitude
+                }
+
+                // Integrate around this axis with the angular speed by the timestep
+                // in order to get a delta rotation from this sample over the timestep
+                // We will convert this axis-angle representation of the delta rotation
+                // into a quaternion before turning it into the rotation matrix.
+                val thetaOverTwo: Float = omegaMagnitude * dT / 2.0f
+                val sinThetaOverTwo: Float = sin(thetaOverTwo)
+                val cosThetaOverTwo: Float = cos(thetaOverTwo)
+                deltaRotationVector[0] = sinThetaOverTwo * axisX
+                deltaRotationVector[1] = sinThetaOverTwo * axisY
+                deltaRotationVector[2] = sinThetaOverTwo * axisZ
+                deltaRotationVector[3] = cosThetaOverTwo
+            }
+            timestamp = event?.timestamp?.toFloat() ?: 0f
+            val deltaRotationMatrix = FloatArray(9) { 0f }
+            SensorManager.getRotationMatrixFromVector(deltaRotationMatrix, deltaRotationVector);
+            // User code should concatenate the delta rotation we computed with the current rotation
+            // in order to get the updated rotation.
+            // rotationCurrent = rotationCurrent * deltaRotationMatrix;
+
+            Log.e("Rotation Vector 1: ", deltaRotationVector[0].toString())
+        }
+
+        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
+
+        }
+    }
+
+    private fun createLocationRequest()
     {
         Log.e("Location:", " Klicano");
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
@@ -78,13 +203,6 @@ class MainActivity : AppCompatActivity() {
             ) != PackageManager.PERMISSION_GRANTED
         ) {
             Log.e("Location:", " Denied");
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
             return
         }
         fusedLocationClient.requestLocationUpdates(
