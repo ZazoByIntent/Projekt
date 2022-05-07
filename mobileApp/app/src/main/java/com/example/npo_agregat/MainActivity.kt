9a18a91e97816eb022ed0bc9dbf1a1b44fc22995
DinoDinoo93 <57443334+DinoDinoo93@users.noreply.github.com>
2022-05-07 15:47:24 +0200

    Lokacija
    Dodana koda za lokacijo, permission ne deluje

--- mobileApp/app/src/main/java/com/example/npo_agregat/MainActivity.kt
+++ mobileApp/app/src/main/java/com/example/npo_agregat/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.npo_agregat
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.app.ActivityCompat
 import com.example.npo_agregat.databinding.ActivityMainBinding
+import com.google.android.gms.location.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     lateinit var app:MyApplication
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback: LocationCallback
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,10 +23,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         app = application as MyApplication
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
         binding.btnStart.setOnClickListener {
             if(binding.tvStatus.text == getString(R.string.textview_status_idle)){
                 binding.tvStatus.text = getString(R.string.textview_status_capturing)
                 // Zacni zajemanje podatkov
+                startLocationUpdates();
             }
         }
         binding.btnStop.setOnClickListener {
@@ -27,4 +39,76 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    private fun getLocationUpdates()
+    {
+        Log.e("Location:", " Klicano");
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        locationRequest = LocationRequest()
+        locationRequest.interval = 50000
+        locationRequest.fastestInterval = 50000
+        locationRequest.smallestDisplacement = 170f // 170 m = 0.1 mile
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY //set according to your app function
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                locationResult ?: return
+
+                if (locationResult.locations.isNotEmpty()) {
+                    // get latest location
+                    val location = locationResult.lastLocation
+
+                    Log.e("Location:", location.longitude.toString());
+                    // use your location object
+                    // get latitude , longitude and other info from this
+                }
+
+
+            }
+        }
+    }
+
+    //start location updates
+    private fun startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            Log.e("Location:", " Denied");
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        fusedLocationClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            null /* Looper */
+        )
+    }
+
+    // stop location updates
+    private fun stopLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+
+    // stop receiving location update when activity not visible/foreground
+    override fun onPause() {
+        super.onPause()
+        stopLocationUpdates()
+    }
+
+    // start receiving location update when activity  visible/foreground
+    override fun onResume() {
+        super.onResume()
+        startLocationUpdates()
+    }
+
 }
