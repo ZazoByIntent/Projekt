5cb8368e54e8066d2dd32f23d42a172b0ed10472
JernejBelcl <91827346+JernejBelcl@users.noreply.github.com>
2022-05-10 12:59:24 +0200

    Scrapper Cas zajemanja in gostota prometa

--- projektScrapper/projektScrapper.js
+++ projektScrapper/projektScrapper.js
@@ -9,18 +9,19 @@ rp(url)
       var lokacija = json.Contents[0].Data.Items[element].stevci_lokacijaOpis;
       var latitude = json.Contents[0].Data.Items[element].y_wgs;
       var longitude = json.Contents[0].Data.Items[element].x_wgs;
+      var cas = json.Contents[0].ModifiedTime;
       console.log("Lokacija: " + lokacija + " na koordinatah: Y:" + latitude + " X: " + longitude);
-      for (var subElement in json.Contents[0].Data.Items[element].Data)
-      {
-        var averageSpeed = json.Contents[0].Data.Items[element].Data[subElement].properties.stevci_hit;
+      console.log("Cas: " + cas);
+    //  for (var subElement in json.Contents[0].Data.Items[element].Data)
+     // {
+        var averageSpeed = json.Contents[0].Data.Items[element].Data[0].properties.stevci_hit;
         console.log("Povprečna hitrost: " + averageSpeed + " km/h");
-        var numberOfVehicles = json.Contents[0].Data.Items[element].Data[subElement].properties.stevci_stev;
+        var numberOfVehicles = json.Contents[0].Data.Items[element].Data[0].properties.stevci_stev;
         console.log("Stevilo vozil: " + numberOfVehicles);
-      }
-      
-      
-
-      
+       var gostotaPrometa = json.Contents[0].Data.Items[element].Data[0].properties.stevci_statOpis;
+        console.log("Gostota: " + gostotaPrometa);
+        console.log("--------------------------------")
+     // }
       
       // Poslji ime lokacije, x in y koordinate
     }
