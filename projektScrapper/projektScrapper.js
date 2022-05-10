4cbb09414b8c3f057bd0510de9952e5797820f66
DinoDinoo93 <57443334+DinoDinoo93@users.noreply.github.com>
2022-05-10 12:25:17 +0200

    Scrapper Povprečna hitrost in število vozil
    Scrapper zdej iz json datoteke za vsako točko prebere povprečno hitrost vozil in število, ko jih je šlo mimo

--- projektScrapper/projektScrapper.js
+++ projektScrapper/projektScrapper.js
@@ -10,6 +10,16 @@ rp(url)
       var latitude = json.Contents[0].Data.Items[element].y_wgs;
       var longitude = json.Contents[0].Data.Items[element].x_wgs;
       console.log("Lokacija: " + lokacija + " na koordinatah: Y:" + latitude + " X: " + longitude);
+      for (var subElement in json.Contents[0].Data.Items[element].Data)
+      {
+        var averageSpeed = json.Contents[0].Data.Items[element].Data[subElement].properties.stevci_hit;
+        console.log("Povprečna hitrost: " + averageSpeed + " km/h");
+        var numberOfVehicles = json.Contents[0].Data.Items[element].Data[subElement].properties.stevci_stev;
+        console.log("Stevilo vozil: " + numberOfVehicles);
+      }
+      
+      
+
       
       
       // Poslji ime lokacije, x in y koordinate
