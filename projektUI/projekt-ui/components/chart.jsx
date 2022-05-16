1e72dc00fb22e091a2c8691acc3d6b4a2ed50378
Aljaž Hafner <87764692+ZazoByIntent@users.noreply.github.com>
2022-05-17 01:02:42 +0200

    Dodan chart,js, definiran osnovni izgled

---
+++ projektUI/projekt-ui/components/chart.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import Chart from 'chart.js/auto';
+import {Bar} from 'react-chartjs-2';
+import { Card, CardHeader, CardBody } from "reactstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const data = {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
+    }]
+};
+  
+
+const MyChart = () => {
+    return (
+        <>
+            <Card className="bg-dark text-white" style={{ width:"40rem" }}>
+                <CardBody>
+                    <h2>Bar Example</h2>
+                    <Bar
+                        data={data}
+                        id="my-chart"
+                    />
+                </CardBody>
+            </Card>
+        </>
+    );
+};
+
+export default MyChart;
--- projektUI/projekt-ui/pages/map.js
+++ projektUI/projekt-ui/pages/map.js
@@ -1,17 +1,24 @@
 import React from "react";
 import dynamic from "next/dynamic";
+import MyChart from "../components/chart"
 
 export default function Home() {
   const MapWithNoSSR = dynamic(() => import("../components/osmMap"), {
     ssr: false
   });
 
+
   return (
-      
     <main>
-      <div id="map">
-        <MapWithNoSSR />
-      </div>
+        <div className="my-container">
+            <div className="map">
+                <MapWithNoSSR/>
+            </div>
+            <div className="chart">
+                <MyChart/>
+            </div>
+        </div>
     </main>
+
   );
 }
