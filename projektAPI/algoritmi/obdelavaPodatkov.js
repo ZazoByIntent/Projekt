3426a16826a897c3731f5e4a94ee4acfc8b4c19d
JernejBelcl <91827346+JernejBelcl@users.noreply.github.com>
2022-05-22 13:09:23 +0200

    Dodano racunanje za rotacijo

--- projektAPI/algoritmi/obdelavaPodatkov.js
+++ projektAPI/algoritmi/obdelavaPodatkov.js
@@ -9,6 +9,7 @@ module.exports = {
         .limit(2)
         .exec(
             function (err, neobdelana_podatka) {
+                //console.log(neobdelana_podatka);
             if (err) {
                 return res.status(500).json({
                     message: 'Error when creating obdelani_podatki',
@@ -16,35 +17,53 @@ module.exports = {
                 });
             }
 
-            var first = neobdelana_podatka[0];        
-            var second = neobdelana_podatka[1];    
+            var first = neobdelana_podatka[0]; // zadnji dobljen podatek
+            var second = neobdelana_podatka[1]; // predzadnji dobljen podatek
+
+
             var end_result = new Obdelani_podatkiModel({
                 pospesek : 0,
                 rotacija : 0,
                 koordinate : first.koordinate,
                 datum : new Date()
             });
-    
+                console.log("CHECK");
             // https://code.tutsplus.com/tutorials/using-the-accelerometer-on-android--mobile-22125
             var timeDiff = (new Date(first.datum).getTime() - new Date(second.datum).getTime());
             end_result.pospesek = Math.abs(first.x_pospesek + first.y_pospesek + first.z_pospesek - second.x_pospesek - second.y_pospesek - second.z_pospesek)/timeDiff * 10000;
-    
+
             // Dodaj racunanje za rotacijo
+            // Vir: https://stackoverflow.com/questions/29255939/calculate-rotation-from-two-coordintes
+            var degreesX = Math.atan2((first.x_rotacija - second.x_rotacija), (first.y_rotacija - second.y_rotacija)) * 180 / Math.PI;
+                if (degreesX < 0.0) degreesX += 360.0;
+            var degreesY = Math.atan2((first.x_rotacija - second.x_rotacija), (first.z_rotacija - second.z_rotacija)) * 180 / Math.PI;
+                if (degreesY < 0.0) degreesY += 360.0;
+            var degreesZ = Math.atan2((first.y_rotacija - second.y_rotacija), (first.z_rotacija - second.z_rotacija)) * 180 / Math.PI;
+                if (degreesZ < 0.0) degreesZ += 360.0;
+
+            end_result.rotacija = Math.max(degreesX,degreesY,degreesZ);
+
+               /* console.log("DegreesX: ",degreesX);
+                console.log("DegreesY: ",degreesY);
+                console.log("DegreesZ: ",degreesZ);
+                console.log("Rotacija - Koncna: ",end_result.rotacija);
+                console.log("First: ",first);
+                console.log("Second: ",second);*/
 
             if(timeDiff > 5000)
             {
                 // Če je časovna razlika med posnetimi podatki večja od 5 sekund, obdelavo zavržemo(ni natančnih izračunov hitrosti)
                 end_result = null;
                 return res.status(500).json({
-                    message: 'Error when creating obdelani_podatki',
+                    message: 'Error when creating obdelani_podatki2',
                     error: err
                 });
             }
-    
             end_result.save(function (err, obdelani_podatki) {
+
                 if (err) {
                     return res.status(500).json({
-                        message: 'Error when creating obdelani_podatki',
+                        message: 'Error when creating obdelani_podatki3',
                         error: err
                     });
                 }
